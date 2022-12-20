cbd369c2dfca80da0628e01a49d92fa550d32687
Tanya Gupta <78593756+Tanya1224@users.noreply.github.com>
2022-12-20 22:38:28 +0530

    Add files via upload

---
+++ deletelast.java
@@ -0,0 +1,35 @@
+package SinglyLinkedList;
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        data=x;
+        next=null;
+    }
+}
+public class deletelast {
+    public static void main(String[] args) {
+        Node head=new Node(10);
+    	head.next=new Node(20);
+    	head.next.next=new Node(30);
+        print(head);
+        head=del(head);
+        print(head);
+    }
+    static Node del(Node head){
+        if(head==null) return null;
+        if(head.next==null) return null;
+        Node curr=head;
+        while(curr.next.next!=null) curr=curr.next;
+        curr.next=null;
+        return head;
+    }
+    public static void print(Node head){
+        Node curr=head;
+        while(curr!=null){
+            System.out.println(curr.data);
+            curr=curr.next;
+        }
+    }
+}
