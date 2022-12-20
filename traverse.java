763c87956027bdec4fc92c8b2a116ae78d7e60c8
Tanya Gupta <78593756+Tanya1224@users.noreply.github.com>
2022-12-20 21:35:54 +0530

    Add files via upload

---
+++ traverse.java
@@ -0,0 +1,27 @@
+package SinglyLinkedList;
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        data=x;
+        next=null;
+    }
+}
+public class traverse {
+    public static void main(String[] args) {
+        Node head=new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        head.next.next.next=new Node(40);
+        print(head);
+    }
+    public static void print(Node head) {
+        Node curr=head;
+        while(curr!=null){
+            System.out.println(curr.data+" ");
+            curr=curr.next;
+        }
+        
+    }
+}
