f884a8f3c19a5857328ae52c0181f3d346b2229f
Tanya Gupta <78593756+Tanya1224@users.noreply.github.com>
2022-12-22 21:34:47 +0530

    Add files via upload

---
+++ insertheadCLL.java
@@ -0,0 +1,38 @@
+package SinglyLinkedList.CircularLinkedList;
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        data=x;
+        next=null;
+    }
+}
+public class insertheadCLL {
+    static Node insert(Node head, int x){
+        Node temp=new Node(x);
+        if(head==null) temp.next=temp;
+        else{
+            Node curr=head;
+            while(curr.next!=head) curr=curr.next;
+            curr.next=temp;
+            temp.next=head;
+        }
+        return temp;
+    }
+    public static void main(String[] args) {
+        Node head=new Node(10);
+        head.next=new Node(20);
+    	head.next.next=new Node(30);
+    	head.next.next.next=head;
+    	head=insert(head,15);
+    	print(head);
+    }
+    static void print(Node head){
+        if(head==null) return;
+        System.out.println(head.data+" ");
+        for(Node r=head.next;r!=head;r=r.next){
+            System.out.println(r.data+" ");
+        }
+    }
+}
